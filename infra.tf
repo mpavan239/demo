c2924c484f21703aa7acb229163bdd6a878a42d6
mpavan239 <129143407+mpavan239@users.noreply.github.com>
2023-03-28 17:25:16 +0530

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -2,8 +2,9 @@
 resource "aws_ecs_service" "demo_service" {
   name            = "demo-service"
   cluster         = demo-cluster
-  task_definition = arn:aws:ecs:ap-south-1:634441478571:task-definition/demo-task-definition:6
+  task_definition = "arn:aws:ecs:ap-south-1:634441478571:task-definition/demo-task-definition:6\n"
   desired_count   = 1
+  ...
 
   deployment_controller {
     type = "ECS"
