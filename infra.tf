0cd219272badc33085ac12686229f57f9834e1f2
mpavan239 <129143407+mpavan239@users.noreply.github.com>
2023-03-28 13:24:37 +0530

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -53,7 +53,7 @@ resource "aws_lb" "demo_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.demo_sg.id]
-  subnets            = [aws_subnet.demo_sub_pub.id]
+  subnets            = [aws_subnet.demo_sub_pub.id, subnet-0fba0d3acb7e5d3d5]
 
   tags = {
     Name = "demo-lb"
@@ -153,7 +153,28 @@ resource "aws_ecs_cluster" "demo_cluster" {
   }
 }
 
-# Create an ECS task definition for the application
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
+
 resource "aws_ecs_task_definition" "demo_task_definition" {
   family                   = "demo-task-definition"
   container_definitions    = jsonencode([{
@@ -173,7 +194,10 @@ resource "aws_ecs_task_definition" "demo_task_definition" {
   memory                   = 512
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
+
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 }
+
 
 
 # Create an ECS service for the application
