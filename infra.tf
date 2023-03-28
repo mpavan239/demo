e476eb4d0ee135e7265306a1a3cbbb282754a12e
mpavan239 <129143407+mpavan239@users.noreply.github.com>
2023-03-28 18:04:47 +0530

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -1,7 +1,7 @@
 # Create an ECS service for the application
 resource "aws_ecs_service" "demo_service" {
   name            = "demo-service"
-  cluster         = aws_ecs_cluster.demo-cluster.id
+  cluster         = "aws_ecs_cluster.demo-cluster.id"
 
   task_definition = "arn:aws:ecs:ap-south-1:634441478571:task-definition/demo-task-definition:6"
   
@@ -12,8 +12,8 @@ resource "aws_ecs_service" "demo_service" {
   }
 
   network_configuration {
-    security_groups = [aws_security_group.sg-0d1ee26c7d1db2194.id]
-    subnets         = [aws_subnet.subnet-01c2744b9cc6583b3.id]
+    security_groups = "aws_security_group.sg-0d1ee26c7d1db2194.id"
+    subnets         = "aws_subnet.subnet-01c2744b9cc6583b3.id"
     
   }
 
